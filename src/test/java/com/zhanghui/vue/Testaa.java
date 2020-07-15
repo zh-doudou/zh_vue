4cdc1b9236d53304fbdc2b4f6a2efa8a8c3b5c40
zhangh <302767241@qq.com>
2020-07-15 22:10:55 +0800

    新添加一个测试方法，来玩玩？

---
+++ src/test/java/com/zhanghui/vue/Testaa.java
@@ -0,0 +1,14 @@
+package com.zhanghui.vue;
+
+public class Testaa {
+
+    public static void main(String[] args) {
+
+        int add = add(1, 6);
+        System.out.println("今天天气不错" + add);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
