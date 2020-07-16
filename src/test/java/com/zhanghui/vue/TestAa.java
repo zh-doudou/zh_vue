33ba7b086cdf9ee9a6200ae6979b9b6c3dd51db0
zhangh <302767241@qq.com>
2020-07-16 21:38:49 +0800

    应学管理v1.0

--- src/test/java/com/zhanghui/vue/TestAa.java
+++ src/test/java/com/zhanghui/vue/TestAa.java
@@ -9,6 +9,7 @@ public class TestAa {
         System.out.println("什么啊");
         System.out.println("还行");
         System.out.println("aaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("你这个大傻子");
     }
 
     public static int add(int a, int b) {
