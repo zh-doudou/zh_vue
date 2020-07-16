77f8dae0d7e2e0f529b63f1e89b9fde0a21892ba
zhangh <302767241@qq.com>
2020-07-16 10:24:10 +0800

    重命名了

--- src/test/java/com/zhanghui/vue/TestAa.java
+++ src/test/java/com/zhanghui/vue/TestAa.java
@@ -1,12 +1,13 @@
 package com.zhanghui.vue;
 
-public class Testaa {
+public class TestAa {
 
     public static void main(String[] args) {
 
         int add = add(1, 6);
         System.out.println("今天天气不错" + add);
         System.out.println("什么啊");
+        System.out.println("还行");
     }
 
     public static int add(int a, int b) {
