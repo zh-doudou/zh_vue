0c278a3f6ec04c32688b886516ccff4d94055964
liruijuanaa <1150482495@qq.com>
2020-07-16 21:57:58 +0800

    添加了 输出语句yo

--- src/test/java/com/zhanghui/vue/TestAa.java
+++ src/test/java/com/zhanghui/vue/TestAa.java
@@ -11,6 +11,7 @@ public class TestAa {
         System.out.println("aaaaaaaaaaaaaaaaaaaaa");
         System.out.println("today");
         System.out.println("hei");
+        System.out.println("yo");
         System.out.println("this");
     }
 
