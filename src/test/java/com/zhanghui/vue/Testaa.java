a8b598957928751cade9b419e6c12ebfb8710711
zhangh <302767241@qq.com>
2020-07-16 10:20:20 +0800

    添加一行有意思的注释

--- src/test/java/com/zhanghui/vue/Testaa.java
+++ src/test/java/com/zhanghui/vue/Testaa.java
@@ -6,6 +6,7 @@ public class Testaa {
 
         int add = add(1, 6);
         System.out.println("今天天气不错" + add);
+        System.out.println("什么啊");
     }
 
     public static int add(int a, int b) {
